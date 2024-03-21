a2a8b0002c7710c23ce0fc2b2585f968eafb2323
Ramon Trindade <ramonbraintrindade@gmail.com>
2024-03-21 12:44:52 -0300

    add carrousel

--- src/app/pages/releases/releases.component.ts
+++ src/app/pages/releases/releases.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../shared/product';
+import { CarouselModule } from 'primeng/carousel';
+import { TagModule } from 'primeng/tag';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-releases',
   standalone: true,
-  imports: [],
+  imports: [CarouselModule, TagModule, ButtonModule, CardModule],
   templateUrl: './releases.component.html',
-  styleUrl: './releases.component.scss'
+  styleUrl: './releases.component.scss',
 })
-export class ReleasesComponent {
+export class ReleasesComponent implements OnInit {
+  products: Product[] = [];
+
+  responsiveOptions: any[] | undefined;
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.productService.getProductsSmall().then((products) => {
+      this.products = products;
+    });
+
+    this.responsiveOptions = [
+      {
+        breakpoint: '1199px',
+        numVisible: 1,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '991px',
+        numVisible: 1,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '769px',
+        numVisible: 1,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '361px',
+        numVisible: 1,
+        numScroll: 1,
+      },
+    ];
+  }
+
 
 }
